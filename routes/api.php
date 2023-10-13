7ab1a569e0a4a4d857bb76e5b3dbfe58f6a79cbc
aguskotakata <arjunaredmi5plus@gmail.com>
2023-10-13 14:09:11 +0700

    finish v3

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Termwind\Components\Dd;
 
 Route::get('/posts',[PostController::class,'index']);
+Route::get('/posts/{id}',[PostController::class,'show']);
+Route::get('/posts2/{id}',[PostController::class,'show2']);
 
 
