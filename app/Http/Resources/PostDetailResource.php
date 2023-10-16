684ac42c4198fd85a1aea62368b6821fb2c59150
Arjuna GTG BGT <118537262+J4ARJUNA@users.noreply.github.com>
2023-10-17 06:06:10 +0700

    ep4

--- app/Http/Resources/PostDetailResource.php
+++ app/Http/Resources/PostDetailResource.php
@@ -15,13 +15,13 @@ class PostDetailResource extends JsonResource
     public function toArray(Request $request): array
     {
         return
-        [
-            'id' =>     $this->id,
-            'title' =>     $this->title,
-            'news' =>     $this->news,
-            'created_at' => date_format($this->created_at, "Y/m/d H:i:s"),
-            'author' =>     $this->author,
-            'writer' =>     $this->whenLoaded(' '),
-        ];
+            [
+                'id' =>     $this->id,
+                'title' =>     $this->title,
+                'news' =>     $this->news,
+                'created_at' => date_format($this->created_at, "Y/m/d H:i:s"),
+                'author' =>     $this->author,
+                'writer' =>     $this->whenLoaded('writer'),
+            ];
     }
 }
