1991b5367a5629f91e306cd23910795d3dda118b
aguskotakata <arjunaredmi5plus@gmail.com>
2023-10-17 10:03:30 +0700

    Ep5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
 use App\Http\Resources\PostDetailResource;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -13,7 +14,7 @@ class PostController extends Controller
     {
         $posts = Post::all();
         //return response()->json(['data'=>$posts]);
-        return PostResource::collection($posts);
+        return PostDetailResource::collection($posts->loadMissing('writer:id,username'));
     }
 
     public function show($id)
@@ -23,9 +24,24 @@ class PostController extends Controller
 
     }
 
-    public function show2($id)
+
+    public function store(Request $request)
     {
-        $post = Post::findOrFail($id);
-        return new PostDetailResource($post);
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news' => 'required',
+        ]);
+
+        $request['author']= Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+
+    }
+
+    public function update(Request $request, $id){
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news' => 'required',
+        ]);
     }
 }
