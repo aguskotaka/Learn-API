91f4edd1379b55d0875e4b432b2df356d7c88cff
Arjuna GTG BGT <118537262+J4ARJUNA@users.noreply.github.com>
2023-10-23 20:57:32 +0700

    Ep 6

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,23 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
-Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
-Route::get('/posts2/{id}', [PostController::class, 'show2']);
-
-
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
-Route::get('/me', [AuthenticationController::class, 'me'])->middleware(['auth:sanctum']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/logout', [AuthenticationController::class, 'logout']);
+    Route::get('/me', [AuthenticationController::class, 'me']);
+    Route::post('/posts',[PostController::class, 'store']);
+    Route::patch('/posts/{id}',[PostController::class, 'update'])->middleware('PostOwner');
+    Route::delete('/posts/{id}',[PostController::class, 'destroy'])->middleware('PostOwner');
+
+    Route::post('/comment', [CommentController::class, 'store']);
+    Route::patch('/comment/{id}', [CommentController::class, 'update'])->middleware('CommentOwner');
+});
